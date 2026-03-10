c2055deb5f562a231585602a8a2f1b36edc92536
marsya505 <marsyazulfazahrani0@gmail.com>
2026-03-10 08:20:31 +0700

    hasil di main program

--- src/percabangan_perulangan/bin/percabangan_perulangan.dart
+++ src/percabangan_perulangan/bin/percabangan_perulangan.dart
@@ -1,4 +1,8 @@
 import 'package:percabangan_perulangan/percabangan_perulangan.dart';
 
 void main() {
-  }
+  int angka = 7; // inisialisasi variabel angka yang ingin diuji
+
+  tampilkanTabelPerkalian(angka); // memanggil fungsi utama yang sudah kita buat di folder lib
+  print('----------------------');
+}
